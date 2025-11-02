487637452f02f08cedf476ad1b4536702727b460
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-11-02 18:16:38 -0400

    added vercel analytics as well as removed all cloudflare junk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Canadian Satellite Viz",
-  description: "Visualize Canadian satellites in orbit with real-time conjunction analysis",
-    generator: 'v0.app'
-}
+  description:
+    "Visualize Canadian satellites in orbit with real-time conjunction analysis",
+  generator: "v0.app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="antialiased">{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
